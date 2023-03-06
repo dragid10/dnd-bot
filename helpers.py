1e16187c60b3e992f7ab1d95253cee05e884d4b4
Alex Oladele <dragid10@gmail.com>
2023-03-05 21:02:43 -0500

    ✨: Fix code and add guards based on tests
    In order to get some tests to pass, some code needed to be fixed

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from enum import Enum, unique
 from typing import List, Tuple
 
+from dateutil.relativedelta import relativedelta, weekdays
 from pytz import timezone
 
 est_tz = timezone("America/New_York")
@@ -22,7 +23,7 @@ class Collections(str, Enum):
 class Weekdays(int, Enum):
     MONDAY = 0
     TUESDAY = 1
-    WENDESAY = 2
+    WEDNESDAY = 2
     THURSDAY = 3
     FRIDAY = 4
     SATURDAY = 5
@@ -41,6 +42,12 @@ class Emojis(str, Enum):
 
 
 def plist(inlist: List) -> str:
+    """
+    Turn list of player names into comma-separated list
+
+    :param inlist: List of player objects
+    :return: comma-separated list of player names
+    """
     if len(inlist) > 0:
         return ", ".join([u["name"] for u in inlist])
     else:
@@ -48,8 +55,18 @@ def plist(inlist: List) -> str:
 
 
 def adjacent_days(dotw: int) -> Tuple[int, int]:
+    """
+    Given an index representing the day of the week, get the adjacent days of the week.
+
+    >>> adjacent_days(1)
+    (0, 2)
+
+    :param dotw: Day of the week represented as an integer
+    :type dotw: int
+    :return: Tuple containing the 2 surrounding days of the week represented as integers
+    """
     if dotw < 0 or dotw > 6:
-        raise ValueError
+        raise ValueError(f"{dotw} is not a valid index of a weekday. Valid values are 0..6")
     days = [i for i in range(len(Weekdays))]
     before = days[(dotw - 1) % len(days)]
     after = days[(dotw + 1) % len(days)]
@@ -72,35 +89,53 @@ def get_next_session_day(session_day: int, session_time: str) -> datetime:
     session_hr = int(session_time.split(":")[0])
     session_min = int(session_time.split(":")[1])
 
-    # If session day is greater than 6, its invalid
-    if session_day > 6:
+    # If session day is less than 0 or greater than 6, its invalid
+    if session_day < 0 or session_day > 6:
         raise ValueError(f"{session_day} is not a valid index of a weekday. Valid values are 0..6")
 
-    # If session hour is greater than 24, its invalid
-    if session_hr > 24:
-        raise ValueError(f"{session_hr} is not a valid time. Valid values are 00..24")
+    # If session hour is less than 0 or greater than 24, its invalid
+    if session_hr < 0 or session_hr > 23:
+        raise ValueError(f"{session_hr} is not a valid hour. Valid values are 00..23")
 
-    # If session min is greater than 24, its invalid
-    if session_min > 24:
-        raise ValueError(f"{session_min} is not a valid time. Valid values are 00..59")
+    # If session min is less than 0 or greater than 59, its invalid
+    if session_min < 0 or session_min > 59:
+        raise ValueError(f"{session_min} is not a valid minute. Valid values are 00..59")
 
     # Get current DT and localize it to EST
-    est_dt = datetime.utcnow().replace(second=0, microsecond=0).astimezone(est_tz)
+    est_dt = est_tz.localize(datetime.utcnow().replace(second=0, microsecond=0))
 
-    # Figure out next session date with time deltas
-    # TODO (3/5/23) [dragid10]: Repplace with dateutils.relativedelta
-    # TODO EX: `curr_date + relativedelta(weekday=weekdays[SESSION_DAY](1))`
-    for i in range(1, 8):
-        ret_sess_day = datetime.utcnow().replace(second=0, microsecond=0).astimezone(est_tz)
-        potential_day: datetime = est_dt + timedelta(days=i)
-        potential_day_dotw = potential_day.weekday()
-
-        if potential_day_dotw == session_day:
-            ret_sess_day = potential_day.replace(
-                hour=int(session_time.split(":")[0]),
-                minute=int(session_time.split(":")[1]))
-            return ret_sess_day
+    ret_sess_day = (est_dt + relativedelta(weekday=weekdays[session_day](1))).replace(hour=session_hr, minute=session_min)
+    return ret_sess_day
 
 
 def callable_username(username: str):
+    """
+    Turn a Discord username into a callable version of itself so that the corresponding user can be notified in Discord
+
+    >>> callable_username("test")
+    <@test>
+    :param username:
+    :return:
+    """
+    if str_null_empty(username):
+        raise ValueError("A valid username was not passed in!")
     return f"<@{username}>".strip()
+
+
+def str_null_empty(input_str: str) -> bool:
+    """
+    Check if string is null or empty
+
+    :param input_str: str to check if its null or empty
+    :return: True is string is null or empty, False otherwise
+    """
+    if input_str is None:
+        return True
+
+    if not isinstance(input_str, str):
+        raise ValueError(f"{input_str} is not a str!")
+
+    input_str = input_str.casefold().strip()
+    if input_str == "":
+        return True
+    return False
